909e312d30a7b2fc63f8a9a6c717c771b26fd3ff
BrayanGomezL <brayangomezlopez2005@gmail.com>
2025-07-05 20:51:11 -0600

    Terminar acceso de datos
    Completar el php de acceso de  datos

--- accessoDatos/accesoDatos.php
+++ accessoDatos/accesoDatos.php
@@ -1,5 +1,32 @@
 <?php
+function abrirConexion(){
 
+try{
 
+    $host = "127.0.0.1";
+    $user = "";
+    $password = "";
+    $db = "BD_AWCS_IIC25";
 
-?>
+    $mysqli = new mysqli($host, $user, $password, $db);
+
+    if($mysqli->connect_error){
+        throw new exception("Sucedió un error al realizar la conexión a la base de datos.");
+    }
+
+    $mysqli->set_charset('utf8mb4');
+
+    return $mysqli;
+
+}catch (Exception $e){
+
+    return false;
+}
+
+}
+
+function cerrarConexion($mysqli){
+    if($mysqli instanceof myqsli){
+        $mysqli->close();
+    }
+}
