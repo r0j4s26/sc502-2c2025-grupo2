5cb98634a64caa0afad77d43aa04811e541b4673
BrayanGomezL <brayangomezlopez2005@gmail.com>
2025-07-09 16:27:50 -0600

    navbar
    Desarrollo de la barra de navegacion de la pagina

--- view/componentes/navbar.php
+++ view/componentes/navbar.php
@@ -1,25 +1,42 @@
 <?php
-
-//HAY QUE EDITAR ESTE NAVBAR Y LLAMARLO DONDE SEA NECESARIO
 echo '
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-            <a class="navbar-brand" href="#">Administrador de Tareas</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PROGRAMA_SEMANA_4/view/usuarios/listaUsuarios.php">Usuarios</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/PROGRAMA_SEMANA_4/view/login.php">Cerrar Sesión</a>
-                    </li>
-                </ul>
-            </div>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #8B0000;">
+    <div class="container-fluid">
+        <a class="navbar-brand fs-4" href="#">Inicio</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContenido" aria-controls="navbarContenido" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarContenido">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="adminDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Administración
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Usuarios</a></li>
+                        <li><a class="dropdown-item" href="#">Categorías</a></li>
+                        <li><a class="dropdown-item" href="#">Repuestos</a></li>
+                        <li><a class="dropdown-item" href="#">Citas</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Citas</a>
+                </li>
+            </ul>
+
+            <form class="d-flex me-3" role="search">
+                <input class="form-control rounded-pill me-2" type="search" placeholder="Buscar..." aria-label="Buscar" style="width: 250px;">
+                <button class="btn btn-outline-light rounded-pill" type="submit">Buscar</button>
+            </form>
+
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Cerrar Sesión</a>
+                </li>
+            </ul>
         </div>
-    </nav>
-'
+    </div>
+</nav>
+';
 ?>
